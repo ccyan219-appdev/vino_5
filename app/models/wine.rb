f9ce983b97b59c98bdf837968d3406f8c3bfeba8
Raghu Betina <raghu@firstdraft.com>
2018-06-17 18:12:50 +0000

    Direct association between wines and ratings

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,6 +1,9 @@
 class Wine < ApplicationRecord
   # Direct associations
 
+  has_many   :ratings,
+             :dependent => :destroy
+
   has_many   :listings,
              :dependent => :destroy
 
